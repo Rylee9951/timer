b35b48e97d307b49dda245f6df15da5ef12490f6
Ryan Lee <RyanLee@Ryans-MacBook-Pro.local>
2016-10-17 16:42:40 -0700

    working on hex

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ setInterval (function(){
   if(hours < 10){
     hours = "0" + hours;
   }
+  
 	var minutes = date.getMinutes()
   if(minutes < 10){
   	minutes = "0" + minutes;
@@ -24,4 +25,33 @@ setInterval (function(){
       
       
   $(".clock").html(time)
+},1000)
+
+setInterval (function(){
+  var date = new Date()
+  var hours = date.getHours()
+  if(hours > 12){
+    hours = hours - "12";
+  }
+  if(hours < 10){
+    hours = "0" + hours;
+  }
+  
+  var minutes = date.getMinutes()
+  if(minutes < 10){
+    minutes = "0" + minutes;
+  }
+  var seconds = date.getSeconds()
+  if(seconds < 10){
+    seconds = "0" + seconds;
+  }
+  var hexs = "#" + hours + minutes + seconds
+  console.log(hexs)
+      
+      
+     
+      
+      
+      
+  $(".hex").html(hexs)
 },1000)
