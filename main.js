7cd6c2c5988440ac6eca2d31290d005449f4b5cc
Ryan Lee <RyanLee@Ryans-MacBook-Pro.local>
2016-10-17 14:20:15 -0700

    clock

---
+++ main.js
@@ -0,0 +1,27 @@
+setInterval (function(){
+	var date = new Date()
+	var hours = date.getHours()
+  if(hours > 12){
+  	hours = hours - "12";
+  }
+  if(hours < 10){
+    hours = "0" + hours;
+  }
+	var minutes = date.getMinutes()
+  if(minutes < 10){
+  	minutes = "0" + minutes;
+  }
+	var seconds = date.getSeconds()
+  if(seconds < 10){
+  	seconds = "0" + seconds;
+  }
+  var time = hours + ":" + minutes + ":" + seconds
+  console.log(time)
+      
+      
+     
+      
+      
+      
+  $(".clock").html(time)
+},1000)
