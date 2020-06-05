7d84f4bec774d9464988c5c39a06ff1fa5d600a1
Ryan Lee <djrlee51@gmail.com>
2020-06-05 10:21:04 -0700

    updating

--- main.js
+++ main.js
@@ -1,60 +1,48 @@
-setInterval (function(){
-	var date = new Date()
-	var hours = date.getHours()
-  if(hours > 12){
-  	hours = hours - "12";
-  }
-  if(hours < 10){
-    hours = "0" + hours;
-  }
-  
-	var minutes = date.getMinutes()
-  if(minutes < 10){
-  	minutes = "0" + minutes;
-  }
-	var seconds = date.getSeconds()
-  if(seconds < 10){
-  	seconds = "0" + seconds;
-  }
-  var time = hours + ":" + minutes + ":" + seconds
-        
-      
-  $(".clock").html(time)
-},1000)
-
-
-
-setInterval (function(){
+setInterval(function() {
   var date = new Date()
   var hours = date.getHours()
-  if(hours > 12){
-    hours = hours - "12";
+  if (hours > 12) {
+    hours = hours - "12"
   }
-  if(hours < 10){
-    hours = "0" + hours;
+  if (hours < 10) {
+    hours = "0" + hours
   }
-  
+
   var minutes = date.getMinutes()
-  if(minutes < 10){
-    minutes = "0" + minutes;
+  if (minutes < 10) {
+    minutes = "0" + minutes
   }
   var seconds = date.getSeconds()
-  if(seconds < 10){
-    seconds = "0" + seconds;
+  if (seconds < 10) {
+    seconds = "0" + seconds
   }
-  var color = "#" + hours + minutes + seconds
-  
-   
+  var time = hours + ":" + minutes + ":" + seconds
 
-  
- 
-  
-  
-  
-  
-    
-      
-    
-      
-  $(".hex").html(color).css("background-color", `${color}`)
-},1000)
+  $(".clock").html(time)
+}, 1000)
+
+setInterval(function() {
+  var date = new Date()
+  var hours = date.getHours()
+  if (hours > 12) {
+    hours = hours - "12"
+  }
+  if (hours < 10) {
+    hours = "0" + hours
+  }
+
+  var minutes = date.getMinutes()
+  if (minutes < 10) {
+    minutes = "0" + minutes
+  }
+  var seconds = date.getSeconds()
+  if (seconds < 10) {
+    seconds = "0" + seconds
+  }
+  var color =
+    "#" + hours.toString(16) + minutes.toString(16) + seconds.toString(16)
+
+  $(".hex")
+    .html(color)
+    .css("background-color", `${color}`)
+}, 300)
